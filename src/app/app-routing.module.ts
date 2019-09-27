7af87bc4db50fa740c377718873aeebd955294fe
Claudia <clau.lorens.23@gmail.com>
2019-09-27 22:01:01 +0200

    feat: basic arquitecture and angular material set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TeamModule } from './team/team.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => TeamModule
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
